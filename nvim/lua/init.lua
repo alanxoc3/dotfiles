dca82d4011e010f2d57a0f4d374a3fec12e92755
Alan Morgan <alanxoc3@gmail.com>
2020-11-08 03:29:49 -0700

    Spell. Concards. Journal aliases. Pandoc publish.
    Just a routine update with all those things.

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -8,16 +8,16 @@ vim.api.nvim_exec([[
    Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
    Plug 'junegunn/fzf.vim'
    Plug 'SirVer/ultisnips'
+   Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
+   Plug 'justinmk/vim-sneak'
 
    " Plug 'svermeulen/vimpeccable'
    " Plug 'posva/vim-vue'
    " Plug 'qpkorr/vim-renamer'
    " Plug 'tpope/vim-fugitive'
    " Plug 'wsdjeg/vim-fetch'
-   " Plug 'justinmk/vim-sneak'
    " Plug 'tpope/vim-surround'
    " Plug 'easymotion/vim-easymotion'
-   " Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
    " Plug 'jamessan/vim-gnupg'
    " Plug 'francoiscabrol/ranger.vim'
    " Plug 'rbgrouleff/bclose.vim'
@@ -35,85 +35,45 @@ vim.g.python_host_prog  = '/usr/bin/python2'
 vim.g.python3_host_prog = '/usr/bin/python3'
 vim.g.buftabline_indicators = 1
 vim.g.buftabline_numbers = 2
-
--- https://shapeshed.com/vim-netrw/
 vim.g.netrw_banner = 0
-
--- ranger plugin config
 vim.g.ranger_replace_netrw = 1
 vim.g.ranger_map_keys = 0
-
 vim.g["asterisk#keeppos"] = 1
 vim.g["deoplete#enable_at_startup"] = 1
 vim.g.mapleader = " "
-
 vim.g.spell_under = "default"
-
--- No annoying message from cwd when screen is too short. See:
 vim.g.rooter_silent_chdir = 1
-
--- go plugin
 vim.g.go_fmt_autosave = 0
-
--- Snippets...
 vim.g.UltiSnipsExpandTrigger = "<tab>"
 vim.g.UltiSnipsJumpForwardTrigger = "<c-b>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<c-z>"
-
--- If you want :UltiSnipsEdit to split your window.
 vim.g.UltiSnipsEditSplit = "vertical"
 
--- https://joshldavis.com/2014/04/05/vim-tab-madness-buffers-vs-tabs/
--- This allows buffers to be hidden if you've modified a buffer.
--- This is almost a must if you wish to use buffers in this way.
 vim.o.hidden = true
-
--- Thanks stack overflow.
 vim.o.joinspaces = false
-
--- no status line
 vim.o.ls = 0
-
-vim.wo.wrap = false
 vim.o.spelllang = "en"
 vim.o.number = false
-
--- http://vim.wikia.com/wiki/Indenting_source_code
-vim.bo.tabstop = 3
-vim.bo.shiftwidth = 3
-vim.bo.expandtab = true
-
 vim.o.tabstop = 3
 vim.o.shiftwidth = 3
 vim.o.expandtab = true
-
--- Number of tabs allowed.
 vim.o.tabpagemax = 100
-
--- More fuzzy searching.
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Arch vimrc file
 vim.o.backspace="indent,eol,start"  -- more powerful backspacing
-
--- Now we set some defaults for the editor
 vim.o.history = 50  -- keep 50 lines of command line history
 vim.o.ruler = true  -- show the cursor position all the time
-
--- Suffixes that get lower priority when doing tab completion for filenames.
--- These are files we are not likely to want to edit or read.
 vim.o.suffixes = ".bak,~,.swp,.o,.info,.aux,.log,.dvi,.bbl,.blg,.brf,.cb,.ind,.idx,.ilg,.inx,.out,.toc,.png,.jpg"
-
--- For vim plugins in neovim.
--- https://wiki.archlinux.org/index.php/Neovim
 vim.o.rtp = vim.o.rtp .. ",/usr/share/vim/vimfiles/"
-
 vim.o.encoding = "utf-8"
 vim.o.fileencoding = "utf-8"
-
--- Some vim java thing.
 vim.o.errorformat="[ERROR] %f:[%l\\,%v] %m"
+
+vim.wo.wrap = false
+
+vim.bo.tabstop = 3
+vim.bo.shiftwidth = 3
+vim.bo.expandtab = true
 
 -- Show object.
 function _G.dump(...)
@@ -131,33 +91,40 @@ vim.api.nvim_exec([[
 
 -- Leader Mappings
 vim.api.nvim_exec([[
-   nmap <leader>1 <Plug>BufTabLine.Go(1)
-   nmap <leader>2 <Plug>BufTabLine.Go(2)
-   nmap <leader>3 <Plug>BufTabLine.Go(3)
-   nmap <leader>4 <Plug>BufTabLine.Go(4)
-   nmap <leader>5 <Plug>BufTabLine.Go(5)
-   nmap <leader>6 <Plug>BufTabLine.Go(6)
-   nmap <leader>7 <Plug>BufTabLine.Go(7)
-   nmap <leader>8 <Plug>BufTabLine.Go(8)
-   nmap <leader>9 <Plug>BufTabLine.Go(9)
-   nmap <leader>0 <Plug>BufTabLine.Go(10)
+   nmap <leader>1 :b 1<CR>:file<CR>
+   nmap <leader>2 :b 2<CR>:file<CR>
+   nmap <leader>3 :b 3<CR>:file<CR>
+   nmap <leader>4 :b 4<CR>:file<CR>
+   nmap <leader>5 :b 5<CR>:file<CR>
+   nmap <leader>6 :b 6<CR>:file<CR>
+   nmap <leader>7 :b 7<CR>:file<CR>
+   nmap <leader>8 :b 8<CR>:file<CR>
+   nmap <leader>9 :b 9<CR>:file<CR>
+   nmap <leader>0 :b 10<CR>:file<CR>
    nmap <leader>y :Ranger<CR>
+
    nmap <leader>es :e ~/.dotfiles/nvim/UltiSnips/all.snippets<CR>
    nmap <leader>ev :e ~/.dotfiles/nvim/lua/init.lua<CR>
    nmap <leader>ec :e ~/.dotfiles/nvim/lua/color.lua<CR>
    nmap <leader>ej :e ~/diary/alan/J_diary.md<CR>
+
    nmap <leader>fd :FZF ~/.dotfiles<CR>
-   nmap <leader>rl :w<CR>:luafile %<CR>
-   nmap <leader>rr :luafile ~/.dotfiles/nvim/lua/init.lua<CR>
-   nmap <leader>pi :PlugInstall<CR>
    nmap <leader>ff :FZF<CR>
    nmap <leader>fF :FZF -q '<cword><CR>
    nmap <leader>fr :Rg<CR>
    nmap <leader>fR :Rgw<CR>
+
+   " status
+   nmap <leader>sf :file<CR>
+
+   nmap <leader>rl :w<CR>:luafile %<CR>
+   nmap <leader>rr :luafile ~/.dotfiles/nvim/lua/init.lua<CR>
+   nmap <leader>pi :PlugInstall<CR>
    nmap <leader>T :enew<cr>
-   nmap <leader>l :bnext<CR>
-   nmap <leader>h :bprevious<CR>
+   nmap <leader>l :bnext<CR>:file<CR>
+   nmap <leader>h :bprevious<CR>:file<CR>
    nmap <leader>bq :bp <BAR> bd #<CR>
+   nmap <leader>bd :bp <BAR> bd #<CR>
    nmap <leader>bl :ls<CR>
    nmap <leader>t :set rnu!<CR>
 
